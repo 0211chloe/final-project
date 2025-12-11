aeb6ea6923182da617b73447d14b562fa23ab184
0211chloe <0211chloe@gmail.com>
2025-12-10 23:29:33 -0800

    made changes

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from functions import movie_recs
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        city = request.form.get("city")
+        period = request.form.get("period")
+        movies = movie_recs(city, period)
+    return render_template("index.html", movies=movies)
